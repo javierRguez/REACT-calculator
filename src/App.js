cdbb1f49d42e3638c0c83c33c09e8ab9f82407fe
javiro93 <jotarodriguez939@gmail.com>
2021-03-11 20:21:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import words from 'lodash.words'
+import Functions from './components/Functions'
+import MathOperations from './components/MathOperations'
+import Numbers from './components/Numbers'
+import Result from './components/Result'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [stack, setStack] = useState("")
+
+  const items = words(stack, /[^-^+^*^/]+/g)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="react-calculator">
+      <Result value={undefined} />
+      <Numbers onClickNumber={(number) => console.log(number)} />
+      <Functions onClickClear={() => console.log('clear')} onClickDelete={() => console.log('delete')} />
+      <MathOperations onClickOperation={(operation) => console.log(operation)} onClickEqual={(equal) => console.log(equal)} />
+    </main>
+  )
 }
 
-export default App;
+export default App
